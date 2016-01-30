8e4c5886ed0936c6e1e29e7f19c2add4f8e3fd56
Marc Olano <olano@ad.umbc.edu>
2016-01-30 17:14:22 -0500

    Initial Test Room Work

---
+++ Assets/Scripts/Classes/TestRoom.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestRoom : MonoBehaviour {
+
+    [SerializeField]
+    protected int timeToComplete;
+    [SerializeField]
+    protected Text timerText;
+    protected float currentTime;
+
+    [SerializeField]
+    protected Collider roomZoneCollider;
+
+    protected bool isComplete;
+    [SerializeField]
+    protected RandomizedBlinkRoutine routine;
+
+    /// <summary>
+    /// Starts the timer for the test
+    /// </summary>
+    protected virtual void StartTimer()
+    {
+        currentTime = timeToComplete;
+        StartCoroutine(UpdateTimer());
+    }
+
+    /// <summary>
+    /// Updates the Timer in a Coroutine. Also handles failstate
+    /// </summary>
+    /// <returns></returns>
+    protected virtual IEnumerator UpdateTimer()
+    {
+        while (!isComplete)
+        {
+            if (currentTime > 0)
+            {
+                yield return new WaitForSeconds(1);
+                currentTime--;
+                timerText.text = string.Format("{0}:{1}:{2}", Mathf.Floor(currentTime / 3600), Mathf.Floor(currentTime / 60), currentTime % 60);
+            }
+            else
+            {
+                timerText.text = string.Format("{0}:{1}:{2}", 0,0,0);
+            }
+        }
+    }
+    /// <summary>
+    /// Called when the timer runs out and the player has failed the test
+    /// </summary>
+    protected virtual void FailState()
+    {
+
+    }
+}
